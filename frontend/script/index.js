13f2da5f037f976c2c27d6c3278c5931cef3ed44
Gal Malka <galmalka121@gmail.com>
2023-01-22 17:40:54 +0200

    update
    roll dice animation
    pirate animation
    end game popup / results

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -5,17 +5,22 @@ const browseMapButton = document.getElementById("browse-btn")
 const rollDiceButton = document.getElementById("rolldice-btn")
 const dice = document.getElementById("dice")
 
-
 let pirateMoving = false
+let alreadyRolled = false
 let prevWidth = window.innerWidth;
 let prevHeight = window.innerHeight;
 
 let intervalID
-let direction = 0
 let screenOrientation = window.matchMedia("(orientation: portrait)")
 let currentIsland = 1
 const screenMultiplyer = 2
 const islands = {}
+const diceImgs = {}
+const diceRollImgs = {}
+let currentImage = 1
+let currentRoll = 1
+let finalNumber
+let mapPositon = {X: 0,Y: 0}
 let initialPostion = {
     1: {
         X: 0, Y: 0
@@ -57,26 +62,6 @@ let precentPosition = {
     }
 }
 
-let pirateAjustments = {
-    1: {
-        X: 0, Y: 0
-    },
-    2: {
-        X: 0 , Y:0
-    },
-    3: {
-        X: 0 , Y: 0
-    },
-    4: {
-        X: 0 , Y: 0
-    },
-    5: {
-        X: 0, Y: 0
-    },
-    6: {
-        X: 0, Y: 0
-    }
-}
 
 const character = document.getElementById('character')
 let characterWidth
@@ -95,6 +80,14 @@ function onLoad(){
     for(i = 1 ; i <= 6; ++i){
         let id = ("island" + ("0" + i))
         islands[i] = document.getElementById(id)
+    }
+    for(i = 1; i <= 6; ++i){
+        let id = ("../assets/dice/dice" + ("0" + i) + ".png")
+        diceImgs[i] = id
+    }
+    for(i = 1; i <=3; ++i){
+        let id = ("../assets/dice/diceroll" + ("0" + i) + ".png")
+        diceRollImgs[i] = id
     }
     detectScreen()
     handleResize()
@@ -119,8 +112,6 @@ function movePirate(key){
     let positionX = islandRect.left + (islandRect.width / 2)
     let positionY = islandRect.top + (islandRect.height / 2)
 
-    
-
     character.style.left = positionX + 'px'
     character.style.top = positionY - characterRect.height + 'px'
 
@@ -135,14 +126,19 @@ function moveMap(){
         let newHeight = Math.min(Math.max(gameScreen.offsetTop + distanceY, -screenHeight ), 0)
         gameScreen.style.left = newWidth + 'px'
         gameScreen.style.top = newHeight + 'px'
-    
+        
+        distanceX = (screenWidth / prevWidth) * distanceX
+        distanceY = (screenHeight / prevHeight) * distanceY
+
         Object.keys(islands).forEach(key =>{
             let width = Math.min(Math.max(islands[key].offsetLeft + distanceX, -screenWidth  + initialPostion[key].X), initialPostion[key].X)
             let height = Math.min(Math.max(islands[key].offsetTop + distanceY, -screenHeight + initialPostion[key].Y), initialPostion[key].Y)
             islands[key].style.left = width + 'px'
             islands[key].style.top = height + 'px'
         })
-    
+        
+        mapPositon.X = newWidth
+        mapPositon.Y = newHeight
         movePirate(currentIsland)
     }
 }
@@ -159,65 +155,130 @@ function browseMap(){
 }
 
 function rollDice(){
-    var randomNum = Math.floor(Math.random() * 6) + 1;
-    dice.innerHTML = randomNum
-    moveAnimationHandler(randomNum)
+    if(!alreadyRolled){
+        currentImage = 1
+        currentRoll = 1
+        intervalID = setInterval(() => {
+            dice.src = diceRollImgs[currentRoll]
+            console.log(currentRoll);
+            currentRoll != 0 ?
+            currentRoll = (currentRoll + 1) % 4
+            : null
+            
+        } , 250)
+    
+        setTimeout(() => {
+            clearInterval(intervalID)
+            finalNumber = Math.floor(Math.random() * 6) + 1
+            dice.src = diceImgs[finalNumber]
+            console.log("final" + (finalNumber));
+            moveAnimationHandler(finalNumber)
+        } , 1000)
+        alreadyRolled = true
+    }
+    
+    
 }
 
 function moveAnimationHandler(island){
-    pirateMoving = true
-    console.log("I:" + island);
-    //4
-    if(island >= 1){
-        console.log(1);
-        moveAnimation(1)
+    if(island == 1){
+        endGameResults()
+    } else {
+        let promise = Promise.resolve()
+        for(i = 2; i <= island; ++i){
+            let curr = i
+            promise = promise.then(() => moveAnimation(curr))
+        }  
     }
-    if(island >= 2){
-        console.log(2);
-        moveAnimation(2)
-    }
-    if(island >= 3){
-        console.log(3);
-        moveAnimation(3)
-    }
-    if(island >= 4){
-        console.log(4);
-        moveAnimation(4)
-    }
-    if(island >= 5){
-        console.log(5);
-        moveAnimation(5)
-    }
-    if(island >= 6){
-        console.log(6);
-        moveAnimation(6)
-    }
-
-    currentIsland = island
-    pirateMoving = false
+    
 }
 
-function moveAnimation(island){
-    intervalID = setInterval(() => {
-        console.log(intervalID);
-        let islandRect = islands[island].getBoundingClientRect()
-        let characterRect = character.getBoundingClientRect()
-        
-        let positionX = islandRect.left + (islandRect.width / 2)
-        let positionY = islandRect.top + (islandRect.height / 2) - characterRect.height
+function moveAnimation(i){
+    return new Promise((resolve) => {
+        pirateMoving = true
+        intervalID = setInterval(() => {
+            let directionX , directionY
+            let diffrenceX , diffrenceY
 
-        if(characterRect.left < positionX){
-            character.style.left = (characterRect.left + 1) + 'px'
-        }
-        if(characterRect.top < positionY){
-            character.style.top = (characterRect.top + 1) + 'px'
-        }
-        
-        if(characterRect.left >= islandRect.left && characterRect.top >= islandRect.top){
-            console.log("Animation stoped");
-            clearInterval(intervalID)
-        }
-    },5)
+            let characterRect = character.getBoundingClientRect()
+            let currentIslandRect = islands[currentIsland].getBoundingClientRect()
+            let destinationIslandRect = islands[i].getBoundingClientRect()
+
+            characterRect.left < destinationIslandRect.left ? directionX = 1 : directionX = -1
+            characterRect.top < destinationIslandRect.top ? directionY = 1 : directionY = -1
+
+            if (!pirateMoving) {
+                currentIsland = i
+                clearInterval(intervalID);
+                resolve()
+            } 
+            else {
+                if(Math.round(characterRect.left) != Math.round(destinationIslandRect.left)){
+                    character.style.left = characterRect.left + directionX + 'px'
+    
+                }
+                if(Math.round(characterRect.top) != Math.round(destinationIslandRect.top)){
+                    character.style.top = characterRect.top + directionY + 'px'
+                }
+
+                if(Math.round(characterRect.left)  == Math.round(destinationIslandRect.left) && Math.round(characterRect.top) == Math.round(destinationIslandRect.top)){
+                    pirateMoving = false
+                    if(i == finalNumber){
+                        enablePopup()
+                    }
+                }
+            }
+        } , 1)
+    })
+
+}
+
+function followCharacter(island){
+    while(pirateMoving){
+        let characterRect = character.getBoundingClientRect()
+        let distance = islands[island]
+    }
+}
+
+
+
+function enablePopup(){
+    let popupHeader = document.getElementById('popup-header')
+    let popupText = document.getElementById('popup-text')
+    popupScreen.style.display = 'flex'
+    
+    switch(finalNumber){
+        case 1:
+            popupHeader.innerHTML = 'You stayed on the island'
+            popupText.innerHTML = 'Game Over!'
+            break
+        case 2:
+            let random = Math.floor(Math.random() * 10) + 1
+            if(random >= 5){
+                popupHeader.innerHTML = 'You drunk poisonous rum!'
+                popupText.innerHTML = 'Game Over!'
+            } else {
+                popupHeader.innerHTML = 'You drunk great rum!'
+                popupText.innerHTML = 'You Win!'
+            }
+            break
+        case 3:
+            popupHeader.innerHTML = 'You fought a dragon and lost'
+            popupText.innerHTML = 'Game Over!'
+            break
+        case 4:
+            popupHeader.innerHTML = 'You found the treasure!'
+            popupText.innerHTML = 'You Win!'
+            break
+        case 5:
+            let message = ''
+            popupText.innerHTML = message
+            break
+        case 6:
+            popupHeader.innerHTML = 'You arrived to the island!'
+            popupText.innerHTML = 'You Win!'
+            break
+    }
 }
 
 screenOrientation.addEventListener("change", (e) =>{
@@ -236,34 +297,18 @@ window.addEventListener('resize', ()=> {
 })
 
 function handleResize(){
-    let characterRect = character.getBoundingClientRect()
-    let sidemenuRect = sidemenuScreen.getBoundingClientRect()
-    let currentWidth = window.innerWidth
-    let currentHeight = window.innerHeight
     screenWidth = window.innerWidth
     screenHeight = window.innerHeight
+
+    distanceX = (screenWidth / prevWidth) * distanceX
+    distanceY = (screenHeight / prevHeight) * distanceY
 
     gameScreen.style.width = (screenWidth * screenMultiplyer) + 'px'
     gameScreen.style.height = (screenHeight * screenMultiplyer) + 'px'
 
-    let charRatio = 0
-    if(characterRect.width > characterRect.height){
-        charRatio = characterRect.width / characterRect.height
-    } else {
-        charRatio = characterRect.height / characterRect.width
-    }
-
-    character.style.width = Math.floor((screenWidth * 0.15)) + 'px'
-    character.style.height = Math.floor((screenHeight * 0.15 * charRatio)) + 'px'
-
-    sidemenuScreen.style.width = Math.floor(screenWidth * 0.2) + 'px'
-    sidemenuScreen.style.height = Math.floor(screenHeight * 0.2 * (sidemenuRect.width / sidemenuRect.height)) + 'px'
+    character.style.width = (screenWidth * screenMultiplyer) * 0.05 + 'px'
 
     Object.keys(islands).forEach(key => {
-        let ration = islands[key].style.width / islands[key].style.height
-        islands[key].style.width = (screenWidth * 0.25)  + 'px'
-        islands[key].style.height = (screenHeight * 0.25 * ration)  + 'px'
-
         initialPostion[key].X = distanceX + (screenWidth * screenMultiplyer) * precentPosition[key].X
         initialPostion[key].Y = distanceY + (screenHeight * screenMultiplyer) * precentPosition[key].Y
 
@@ -271,8 +316,8 @@ function handleResize(){
         islands[key].style.top = initialPostion[key].Y + 'px'
     })
 
-    prevWidth = currentWidth
-    prevHeight = currentHeight
+    prevWidth = screenWidth
+    prevHeight = screenHeight
 }
 
 window.addEventListener("mousedown", function(event) {
@@ -288,9 +333,14 @@ window.addEventListener("mousemove", function(event) {
       var currentY = event.clientY;
       distanceX = currentX - startX
       distanceY = currentY - startY
-      //distance += Math.sqrt(Math.pow(currentX - startX, 2) + Math.pow(currentY - startY, 2));
       if(enableBrowseMap)
         moveMap()
     }
   });
   
+
+
+  // Known Bugs
+  // 1. When pirate moving change resize is bugged.
+  // 2. When you move the map and than resize the map tears
+  // 3. Fix popup text on resize
